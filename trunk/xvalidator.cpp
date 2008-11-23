ca9b7161ba14c1301d0daaabbef7e20afbaebea4
lukefei <lukefei@91aca6be-b770-11dd-bd25-4dde899625b4>
2008-11-23 22:44:31 +0000

    Adding xvalidator.h/cpp into repository.

---
+++ trunk/xvalidator.cpp
@@ -0,0 +1,155 @@
+/**
+ * \file xvalidator.cpp
+ * \author Kefei Lu
+ * \brief Implementation of cross validator.
+ */
+#include "common.h"
+#include "xvalidator.h"
+
+Xvalidator::
+Xvalidator(Classifier* c, const size_t f, RSeed s)
+{
+    seed() = s;
+    fold() = f;
+    _binded_classifier = c;
+    init_randomIndex();
+}
+
+void
+Xvalidator::
+init_randomIndex()
+{
+    randomIndecs().clear();
+    randomIndecs().resize(fold());
+    const size_t nInst = classifier().dataset().num_of_inst();
+    const size_t len = nInst / fold();
+    const size_t len_last = len + nInst % fold();
+    for ( size_t i=0;i<fold();i++ ) {
+	randomIndecs().at(i).clear();
+	if (i == fold()-1) {
+	    randomIndecs().at(i).resize(len_last,0);
+	} else {
+	    randomIndecs().at(i).resize(len,0);
+	}
+    }
+}
+
+void 
+Xvalidator::randomize(void) 
+{
+    fprintf(stdout, "(I) Randomizing the instances...");
+    const size_t nInst = classifier().dataset().num_of_inst();
+    vector<size_t> unused(nInst);
+    for (size_t i=0;i<nInst;i++) {
+	unused.at(i) = i;
+    }
+
+    vector<size_t> ran(nInst,0);
+    size_t tmp = 0;
+    // randomize the ran vector
+    for (size_t i=0;i<nInst;i++) {
+	tmp = rand() % (nInst-i);
+	ran.at(i) = tmp;
+	// remove the tmp-th element from unused:
+	unused.erase(unused.begin()+tmp);
+    }
+
+    { // Assign ran vectors to the randomIndecs
+	size_t i = 0;
+	size_t j = 0;
+	size_t k = 0;
+	for (i=0;i<fold();i++) { // i-th randomIndecs vector
+	    j = 0; // keep the current position in i-th randomIndecs vector
+	    while ( j < randomIndecs().at(i).size() ) {
+		randomIndecs().at(i).at(j) = ran.at(k);
+		k++; // keep the current position in ran vector.
+		j++;
+	    }
+	}
+    }
+    // Sort every randomIndecs vector.
+    for (size_t i=0;i<fold();i++) {
+	sort(randomIndecs().at(i).begin(),randomIndecs().at(i).end());
+    }
+}
+
+static
+double double_sum(const double v1, const double v2)
+{
+    return v1+v2;
+}
+
+static
+size_t size_t_sum(const size_t v1, const size_t v2)
+{
+    return v1+v2;
+}
+
+static
+vector<size_t>& vector_sum(vector<size_t>& v1, const vector<size_t>& v2)
+{
+    transform( v1.begin(), v1.end(), v2.begin(), v1.begin(), size_t_sum );
+    return v1;
+}
+
+void 
+Xvalidator::
+xvalidate()
+{
+    srand(seed());
+    randomize();
+    Classifier& c = classifier();
+    const size_t nInst = c.dataset().num_of_inst();
+    const size_t nClass = c.get_class_desc().possible_value_vector().size();
+
+    // Performance tmp:
+    double sum_acc = 0.0; //sum of accuracy
+    double ave_acc = 0.0; 
+    vector<double> sum_trust(nClass,0.0); // sum of trust.
+    vector<double> ave_trust(nClass,0.0);
+    ConfMatr sum_conf(nClass, vector<size_t>(nClass,0)); // sum of conf mat.
+    ConfMatr ave_conf(nClass, vector<size_t>(nClass,0));
+
+    for (size_t foldi = 0; foldi<fold(); foldi++) {
+	c.test_set().clear();
+	c.train_set().clear();
+	// assign test set.
+	c.test_set() = randomIndecs().at(foldi);
+	//assign train set.
+	c.train_set().resize(nInst-c.test_set().size());
+	vector<size_t>::iterator it = c.train_set().begin();
+	for (size_t i=0; i<fold(); i++) { // for each fold
+	    if (i==foldi) continue;
+	    const vector<size_t> & curFold = randomIndecs().at(i);
+	    copy( curFold.begin(), curFold.end(), it );
+	    it += curFold.size();
+	}
+	c.train();
+	c.test();
+	// stores the performance:
+	sum_acc += c.accuracy();
+	transform ( sum_trust.begin(), sum_trust.end(),
+		c.trust().begin(), sum_trust.begin(), double_sum );
+	transform ( sum_conf.begin(), sum_conf.end(),
+		c.conf().begin(), sum_conf.begin(), vector_sum );
+    }
+    // average on the performance:
+    ave_acc = sum_acc/fold();
+    for (size_t i=0;i<nClass;i++) {
+	ave_trust.at(i) = sum_trust.at(i) / fold();
+    }
+    for (size_t i=0;i<nClass;i++) {
+	for (size_t j=0;j<nClass;j++) {
+	    ave_conf.at(i).at(j) = sum_conf.at(i).at(j) / fold();
+	}
+    }
+
+#ifdef __XVALIDATOR_DEBUG__
+    fprintf(stdout, "(I) The cross validation output:\n");
+    fprintf(stdout, "(I) ===========================\n");
+    fprintf(stdout, "(I) The average accuracy: %g\n", ave_acc);
+    show_conf(c,ave_conf);
+    show_trust(c,ave_trust);
+#endif
+}
+
---
+++ trunk/xvalidator.h
@@ -0,0 +1,56 @@
+/**
+ * \file xvalidator.h
+ * \author Kefei Lu
+ * \brief Class structure description on cross validation for classifiers.
+ */
+#ifndef __XVALIDATOR_H__
+#define __XVALIDATOR_H__
+#include "common.h"
+#include "dataset.h"
+#include "classifier.h"
+
+/**
+ * The cross validation structure.
+ *
+ */
+class Xvalidator {
+    private:
+	RSeed		_seed;
+	Classifier *	_binded_classifier;
+	size_t		_fold;
+
+	/** Randomized instance indecs. 
+	 *
+	 * It must have _fold vectors, each stores a portion (nInst/_fold, 
+	 * except the last one.) of instance indecs, unique from the others.*/
+	vector< vector<size_t> > _randomIndecs;
+    public:
+	RSeed& seed() {return _seed;}
+	const RSeed& seed() const {return _seed;}
+	Classifier & classifier() const {return *_binded_classifier;}
+	size_t & fold() {return _fold;}
+	const size_t & fold() const {return _fold;}
+
+	Xvalidator(Classifier* c, const size_t fold = 3, RSeed seed=0);
+
+	vector< vector<size_t> >& randomIndecs() {return _randomIndecs;}
+	const vector< vector<size_t> >& randomIndecs() const {return _randomIndecs;}
+	/** Initialize _randomIndecs.
+	 *
+	 * It must have _fold vectors, each stores a portion (nInst/_fold, 
+	 * except the last one.) of instance indecs, unique from the others.
+	 *
+	 * \sa _randomIndecs */
+	void init_randomIndex();
+
+	/** Randomize the randomIndecs.
+	 *
+	 * It puts (sorted) randomized indecs into the _randomIndecs vectors.
+	 * It must be done before the whole cross validation 
+	 * process, but NOT during the process. */
+	void randomize();
+
+	void xvalidate();
+};
+
+#endif
